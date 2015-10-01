371e3426936adf89fa94d71e8dc8504a66d5c612
Klint Tiley <kctiley@gmail.com>
2015-10-01 14:27:48 -0600

    Fixed problem with v1 to v3 in NYT url

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ var unirest = require('unirest');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
 require('dotenv').load();
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/books', function(req, res) {
+  console.log( "GET /books now firing.....")
     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
       var NYTBooks = response.body.results.books;
